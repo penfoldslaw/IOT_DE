40b64762b71642aafedc12c69aee824b8fddeacc
penfoldslaw <penfoldslaws@gmail.com>
2024-08-18 21:14:03 -0400

    delete test.sh

--- src_stream/test.sh
+++
@@ -1,8 +0,0 @@
-config_file="../src_stream/config.cfg"
-
-topic_1=$(awk -F ' = ' '/topic_1/ {print $2}' "$config_file")
-topic_2=$(awk -F ' = ' '/topic_2/ {print $2}' "$config_file")
-
-
-echo "$topic_1"
-echo "$topic_2"
